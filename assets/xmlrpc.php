39e608521a5656396f2de5559ff8513d926d2db2
backup <backup@local>
2026-09-16 17:34:11 -0300

    Estado original antes da adaptação

---
+++ assets/xmlrpc.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://yummekids.com.br</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://yummekids.com.br/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://yummekids.com.br/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://yummekids.com.br/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://yummekids.com.br/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://yummekids.com.br/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
